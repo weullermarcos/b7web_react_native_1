a0d5db27f76f063a37281689c1787237e7c427a8
Weuller <weullermarcos@hotmail.com>
2024-02-27 15:26:44 -0300

    Ações de toque com Pressable

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Alert, Button, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Alert, Button, Pressable, SafeAreaView, StyleSheet, Text, View } from "react-native";
 import Constants from 'expo-constants';
 
 //importando componente PizzaItem criado
@@ -12,6 +12,24 @@ const App = () =>{
       Alert.alert('Fui clicado!');
    }
 
+   //Ação para quando COMEÇOU a apertar o botão
+   const handleComecouApertar = () => {
+
+      Alert.alert('Começou a apertar!');
+   }
+
+   //Ação para quando TERMINOU de apertar o botão
+   const handleTerminouApertar = () => {
+      
+      Alert.alert('Terminou de apertar!');
+   }
+
+   //Ação para quando fez um Aperto Longo
+   const handleApertoLongo = () => {
+
+      Alert.alert('Aperto Longoooo!');
+   }
+
    return (
    <SafeAreaView style={{ margin: Constants.statusBarHeight }}>
 
@@ -20,20 +38,36 @@ const App = () =>{
 
       <Button title="Clique aqui!" onPress={handleButton} />
 
+      <Pressable style={styles.button} onPressIn={handleComecouApertar} onPressOut={handleTerminouApertar}>
+         <View>
+            <Text style={styles.buttonText}>Clique em mim!</Text>
+         </View>
+      </Pressable>
+
+      <Pressable style={styles.button} onLongPress={handleApertoLongo} delayLongPress={1000}>
+         <View>
+            <Text style={styles.buttonText}>Long Press!</Text>
+         </View>
+      </Pressable>
+
 
       <Text style={styles.subtitulo}>Lista de Pizzas:</Text>
       
-      <PizzaItem 
-         name="Calabresa" 
-         price={20} 
-         originalPrice={100}
-         items={['Farinha', 'Cebola', 'Queijo', 'Calabresa', 'Corante']}
-      />
+      <Pressable onPress={handleButton}>
+         <PizzaItem 
+            name="Calabresa" 
+            price={20} 
+            originalPrice={100}
+            items={['Farinha', 'Cebola', 'Queijo', 'Calabresa', 'Corante']}
+         />
+      </Pressable>
+
       <PizzaItem 
          name="Portuguesa" 
          price={50} 
          items={['Cebola', 'Queijo', 'Calabresa', 'Pimentão']}
       />
+
       <PizzaItem 
          name="Frango com Pequi" 
          price={150} 
@@ -56,7 +90,18 @@ const styles = StyleSheet.create({
       fontSize: 18,
       color: '#0000FF',
       textAlign: 'center',
-   }
+   },
+
+   button:{
+      backgroundColor: '#FF0000',
+      padding: 10,
+      margin: 10,
+   },
+
+   buttonText:{
+      color: '#FFFFFF',
+      textAlign: 'center',
+   },
 });
 
 export default App;
